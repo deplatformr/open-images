dee69baf4f11ee2f92565502f888179ad8075a99
Peter Van Garderen <peter@artefactual.com>
2020-10-08 20:50:04 -0700

    WIP: segmentation zip files download script

---
+++ pipeline/scripts/download_segmentations.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+download_dir = os.path.join(os.getcwd, "../source_data/segmentations")
+urls = ("https://storage.googleapis.com/openimages/v5/train-masks/train-masks-0.zip",
+        "https://storage.googleapis.com/openimages/v5/train-masks/train-masks-1.zip",
+        "https://storage.googleapis.com/openimages/v5/train-masks/train-masks-2.zip")
+
+for url in urls:
+
+    split = os.split(url)
+    file = split[1]
+    filepath = os.path.join(download_dir, file)
+
+    response = requests.get(url, stream=True, timeout=(3, 10))
+    file = open(filepath, "wb")
+    response.raw.decode_content = True
+    shutil.copyfileobj(response.raw, file)

fd337b09ecce824b4c94cc239583d2cbdfdd608a
peterVG <peter@ip-172-31-26-31.us-east-2.compute.internal>
2020-10-09 03:59:15 +0000

    update seg download script

--- pipeline/scripts/download_segmentations.py
+++ pipeline/scripts/download_segmentations.py
@@ -1,14 +1,15 @@
 import requests
 import os
+import shutil
 
-download_dir = os.path.join(os.getcwd, "../source_data/segmentations")
+download_dir = os.path.join(os.getcwd(), "source_data/segmentations")
 urls = ("https://storage.googleapis.com/openimages/v5/train-masks/train-masks-0.zip",
         "https://storage.googleapis.com/openimages/v5/train-masks/train-masks-1.zip",
         "https://storage.googleapis.com/openimages/v5/train-masks/train-masks-2.zip")
 
 for url in urls:
 
-    split = os.split(url)
+    split = os.path.split(url)
     file = split[1]
     filepath = os.path.join(download_dir, file)
 

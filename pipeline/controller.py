7eb12c76c11f8101b4e7819aa8e9bb57dff54e81
Peter Van Garderen <peter@artefactual.com>
2020-10-09 14:20:22 -0700

    fix typo

--- pipeline/controller.py
+++ pipeline/controller.py
@@ -188,7 +188,8 @@ if __name__ == "__main__":
 
     for i in range(1, 20):
         download()
-    for in range(1, 5):
+
+    for i in range(1, 5):
         verify()
         extract()
         sidecar()

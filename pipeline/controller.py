e827420b1378777511cad0c0bbee63268a82c3b6
Peter Van Garderen <peter@artefactual.com>
2020-10-09 14:19:41 -0700

    configiure qa workflow

--- pipeline/controller.py
+++ pipeline/controller.py
@@ -186,12 +186,12 @@ if __name__ == "__main__":
 
     package()
 
-    """
-    download()
-    verify()
-    extract()
-    sidecar()
-    segmentations()
-    batch()
-    package()
-    """
+    for i in range(1, 20):
+        download()
+    for in range(1, 5):
+        verify()
+        extract()
+        sidecar()
+        segmentations()
+        batch()
+        package()

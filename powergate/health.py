05285116e1e995fbbfeb5635ec4618fd08fe6956
Peter Van Garderen <peter@artefactual.com>
2020-10-09 23:51:31 -0700

    troubleshoot api

--- powergate/health.py
+++ powergate/health.py
@@ -1,9 +1,9 @@
 import os
 from pygate_grpc.client import PowerGateClient
 
-api = os.getenv('$POWERGATE_API')
-ffs = os.getenv('$POWERGATE_FFS')
-token = os.getenv('$POWERGATE_TOKEN')
+api = os.getenv('POWERGATE_API')
+ffs = os.getenv('POWERGATE_FFS')
+token = os.getenv('POWERGATE_TOKEN')
 
 print(api)
 print(ffs)

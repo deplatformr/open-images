e0538b36947478357349fadf2756dddedc4a04bb
Peter Van Garderen <peter@artefactual.com>
2020-10-09 23:57:33 -0700

    powergate configs

--- powergate/default_config.py
+++ powergate/default_config.py
@@ -2,11 +2,9 @@ import os
 from pygate_grpc.client import PowerGateClient
 
 api = os.getenv('POWERGATE_API')
+powergate = PowerGateClient(api, False)
 ffs = os.getenv('POWERGATE_FFS')
 token = os.getenv('POWERGATE_TOKEN')
 
-powergate = PowerGateClient(api, False)
-
-print("Checking node health...")
-healthcheck = powergate.health.check()
-print(healthcheck)
+defaultConfig = powergate.ffs.default_config(token)
+print(defaultConfig)

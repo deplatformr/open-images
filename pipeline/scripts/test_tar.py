7a2511162e328b99835f8328db58b3560856d825
Peter Van Garderen <peter@artefactual.com>
2020-10-09 19:52:51 -0700

    test packages

---
+++ pipeline/scripts/test_tar.py
@@ -0,0 +1,19 @@
+import tarfile
+
+
+external_identifier = "deplatformer-open-images-1"
+batch_dir = "source_data/batches/1"
+
+# Create the tar package
+packages_dir = os.path.join(
+    os.getcwd(), "source_data/packages/")
+tarball_name = external_identifier + ".tar"
+tarball = tarfile.open(os.path.join(
+    packages_dir, tarball_name), "w")
+tarball.add(batch_dir)
+tarball.close()
+print("created a tarball")  # debug
+except Exception as e:
+print("Unable to create a tarball package from batch.")
+print(e)
+return()

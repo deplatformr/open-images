de359224db17edd62d6bcfd97d02b018aff98499
Peter Van Garderen <peter@artefactual.com>
2020-10-10 23:25:39 -0700

    test script

--- pipeline/scripts/dbase_test.py
+++ pipeline/scripts/dbase_test.py
@@ -7,9 +7,8 @@ workflow_db = sqlite3.connect(db_path)
 cursor = workflow_db.cursor()
 cursor.execute(
     "SELECT name FROM packages WHERE cid IS NOT NULL LIMIT ?", (1,),)
-    result = cursor.fetchone()
-    if result is None:
-        print("nothing found")
-        return()
-    else:
-        print("Uploading package " + result[0] + " to Filecoin.")
+result = cursor.fetchone()
+if result is None:
+    print("nothing found")
+else:
+    print("Uploading package " + result[0] + " to Filecoin.")

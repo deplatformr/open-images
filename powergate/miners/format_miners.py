34f1deeb84c64f91bfcbf6392d97208aa1c6470d
Peter Van Garderen <peter@artefactual.com>
2020-10-17 20:38:36 -0700

    update miners lists'

--- powergate/miners/format_miners.py
+++ powergate/miners/format_miners.py
@@ -14,39 +14,40 @@ with open("functioning_miners.txt", "r") as miners_list:
 with open("miner_asks.txt", "r") as miners_list:
     lines = miners_list.readlines()
     lines_length = len(lines)
+    print(str(lines_length) + " miners")
     for i in range(0, lines_length):
-        if lines[i][:4] == "Ask:":
-            # Make sure miner can accomodate 1 GiB+ packages
-            if lines[i + 3] == "Max Piece size: 1 MiB\n":
-                continue
-            split = lines[i].split(" ")
-            miner = split[1][:-1]
-            split = lines[i + 1].split(": ")
-            price = split[1][:-5]
-            if miner in cheap_working_miners:
-                cheap_working_miners[miner] = price
+        split = lines[i].split(" ")
+        miner = split[0]
+        line = lines[i][8:]
+        split = line.split(" ")
+        price = int(split[0])
+
+        if miner in cheap_working_miners:
+            cheap_working_miners[miner] = price
 
 # Match functioning miners list to ask price list
 pop_list = []
 for key, value in cheap_working_miners.items():
-    if cheap_working_miners[key] == None:
+    if cheap_working_miners[key] is None:
         pop_list.append(key)
 for empty_value in pop_list:
     cheap_working_miners.pop(empty_value)
+
 sorted_miners = sorted(cheap_working_miners.items(),
                        key=operator.itemgetter(1))
 
 # Print ranked list
 for i in range(0, len(sorted_miners)):
     print(str(i + 1) + ". " + sorted_miners[i]
-          [0] + ": " + sorted_miners[i][1] + " FIL")
+          [0] + ":\t" + str(sorted_miners[i][1]) + " attoFIL")
 
 # Set maximum client price for storage deals
-maximum_price = 0.0000000005
+maximum_price = 500000000
 
 miners = []
 for miner in sorted_miners:
-    if float(miner[1]) <= maximum_price:
+    price = miner[1]
+    if price <= maximum_price:
         miners.append(miner[0])
 
 # Print miners.json

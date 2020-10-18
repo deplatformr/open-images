ff29c0c71474fe966bd4cf23f793ac838d07bbfa
Peter Van Garderen <peter@artefactual.com>
2020-10-18 09:59:35 -0700

    update jobs status in db

--- powergate/deal_status.py
+++ powergate/deal_status.py
@@ -26,7 +26,8 @@ for cid in cids:
     print(cid[1] + " - " + cid[0] + " has " + str(count[0]) + " active deals.")
 
     # TAKE SOME ACTION BASED ON NUMBER OF ACTIVE DEALS
-    # CANCEL IF TOO MANY
+
+    # SHOW CANCEL COMMAND IF TOO MANY
     """ All >= & jobs cancelled on Oct 17 21:00
     if count[0] >= 7:
         cursor.execute("SELECT job_id FROM jobs WHERE cid = ?", (cid[0],),)
@@ -34,32 +35,32 @@ for cid in cids:
         for job in jobs:
             print("POW_SERVERADDRESS=" + api +
                   " pow ffs cancel " + job[0] + " -t " + token)
-    """
-    # CANCEL IF TOO LITTLE, THEN REPUSH
+
+    # SHOW CANCEL COMMAND IF TOO FEW
     if count[0] < 5:
-        interval = 30
         package = cid[1]
         cid = cid[0]
-        if cid != "QmPg9bg8DGMALcGAHET2pFyNPbJUznRJLaCMhyeM9Ncoxp" and cid != "Qmc57MM3wwGDCra8b35g4fAgvcSM3rcbhGizHm44ggLJVk":
-            """
-            cursor.execute("SELECT job_id FROM jobs WHERE cid = ?", (cid[0],),)
-            jobs = cursor.fetchall()
-            if len(jobs) > 0:
-                for job in jobs:
-                    print("POW_SERVERADDRESS=" + api +
-                          " pow ffs cancel " + job[0] + " -t " + token)
-            """
-            utctime = datetime.utcnow()
+    
+        cursor.execute("SELECT job_id FROM jobs WHERE cid = ?", (cid[0],),)
+        jobs = cursor.fetchall()
+        if len(jobs) > 0:
+            for job in jobs:
+                print("POW_SERVERADDRESS=" + api +
+                      " pow ffs cancel " + job[0] + " -t " + token)
 
-            job = powergate.ffs.push(cid, token, override=True)
-            print("Repushed " + package + " - " + cid + " to Filecoin.")
-            print("Job ID: " + job.job_id)
-            utctime = datetime.utcnow()
-            cursor = workflow_db.cursor()
-            cursor.execute("INSERT INTO jobs (job_id, cid, ffs, timestamp, status) VALUES (?,?,?,?,?)",
-                           (job.job_id, cid, ffs, utctime, "Executing",),)
-            workflow_db.commit()
-            print("Waiting " + str(interval) + " seconds before next push.")
-            time.sleep(interval)
+        # REPUSH IF TOO FEW
+        interval = 30
+        utctime = datetime.utcnow()
+        job = powergate.ffs.push(cid, token, override=True)
+        print("Repushed " + package + " - " + cid + " to Filecoin.")
+        print("Job ID: " + job.job_id)
+        utctime = datetime.utcnow()
+        cursor = workflow_db.cursor()
+        cursor.execute("INSERT INTO jobs (job_id, cid, ffs, timestamp, status) VALUES (?,?,?,?,?)",
+                       (job.job_id, cid, ffs, utctime, "Executing",),)
+        workflow_db.commit()
+        print("Waiting " + str(interval) + " seconds before next push.")
+        time.sleep(interval)
 
 workflow_db.close()
+"""
--- powergate/update_jobs.py
+++ powergate/update_jobs.py
@@ -1,24 +1,44 @@
 import os
 import sqlite3
-from datetime import datetime
+from pygate_grpc.client import PowerGateClient
+from google.protobuf.json_format import MessageToDict
+
+
+api = os.getenv('POWERGATE_API')
+ffs = os.getenv('POWERGATE_FFS')
+token = os.getenv('POWERGATE_TOKEN')
+
+
+powergate = PowerGateClient(api, is_secure=True)
 
 
 abs_path = os.getcwd()
 split = os.path.split(abs_path)
-
 workflow_db_path = os.path.join(
     split[0], "pipeline/deplatformr_open_images_workflow.sqlite")
 
 workflow_db = sqlite3.connect(workflow_db_path)
 cursor = workflow_db.cursor()
 
-utctime = datetime.utcnow()
+cursor.execute("SELECT name, cid FROM packages WHERE cid IS NOT NULL")
+packages = cursor.fetchall()
 
-with open("updated_jobs.txt", "r") as jobs_list:
-    jobs = jobs_list.readlines()
+for package in packages:
+    cursor.execute("SELECT job_id FROM jobs WHERE cid=?", (package[1],),)
+    jobs = cursor.fetchall()
+    print(package[0] + "(CID: " + package[1] +
+          ") has " + str(len(jobs)) + " jobs.")
     for job in jobs:
-        split = job.split(",")
-        cursor.execute("UPDATE jobs set job_id=?, timestamp=?, status=? WHERE cid=?",
-                       (split[1], utctime, "Executing", split[0],),)
+        job_id = job[0]
+        print("Job ID: " + job_id)
+        try:
+            response = powergate.ffs.get_storage_job(
+                jid=job_id, token=token)
+            job = MessageToDict(response)
+            status = job["job"]["status"]
+        except Exception as e:
+            status = "JOB_STATUS_NOT_FOUND"
+        print("Status: " + status)
+        cursor.execute(
+            "UPDATE jobs SET status=? WHERE job_id=?", (status, job_id, ), )
         workflow_db.commit()
-workflow_db.close()

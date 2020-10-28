138bc2b7639f455048822d21c7e983ff1f8a8c32
Peter Van Garderen <peter@artefactual.com>
2020-10-28 11:38:54 -0700

    update SSL setting for new Powergate server

--- pipeline/scripts/upload.py
+++ pipeline/scripts/upload.py
@@ -16,7 +16,7 @@ def filecoin_upload(package):
         api = os.getenv('POWERGATE_API')
         ffs = os.getenv('POWERGATE_FFS')
         token = os.getenv('POWERGATE_TOKEN')
-        powergate = PowerGateClient(api, is_secure=True)
+        powergate = PowerGateClient(api, is_secure=False)
 
         db_path = os.path.join(
             abs_path, "deplatformr_open_images_workflow.sqlite")

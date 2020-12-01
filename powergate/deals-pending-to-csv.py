afb3bfb2bb78b6085f0683fa7b68f99574ebe10c
Peter Van Garderen <peter@artefactual.com>
2020-12-01 14:47:42 -0800

    update deal reports to match Powergate 1.x output

--- powergate/deals-pending-to-csv.py
+++ powergate/deals-pending-to-csv.py
@@ -1,5 +1,4 @@
 import os
-import sqlite3
 import csv
 from datetime import datetime
 from pygate_grpc.client import PowerGateClient
@@ -11,18 +10,17 @@ powergate = PowerGateClient(api, False)
 
 # get final storage deals info
 storage_deals = powergate.deals.storage_deal_records(
-    include_pending=False, include_final=True, token=token
+    include_pending=True, include_final=False, token=token
 )
 
 total_deals = len(storage_deals)
-print(str(total_deals) + " finalized storage deals found.")
+print(str(total_deals) + " pending storage deals found.")
 
 if total_deals > 0:
-
     today = datetime.today()
     date_string = today.strftime("%b-%d-%Y")
     wallet_alias = storage_deals[1]["address"][-6:]
-    filename = "finalized-deals-" + wallet_alias + "-" + date_string + ".csv"
+    filename = "pending-deals-" + wallet_alias + "-" + date_string + ".csv"
     print("Writing to " + filename)
 
     with open(filename, 'w') as csvfile:
@@ -31,13 +29,6 @@ if total_deals > 0:
         # write column headers
         csvwriter.writerow(['Deal ID', 'CID', 'Miner ID', 'Filename', 'Size (bytes)', 'Date stored (UTC)',
                             'Piece CID', 'Price per epoch', 'Start epoch', 'Activation Epoch', 'Duration', 'Wallet', 'Deal state'])
-
-        abs_path = os.getcwd()
-        split = os.path.split(abs_path)
-        db_path = os.path.join(
-            split[0], "pipeline/deplatformr_open_images_workflow.sqlite")
-        workflow_db = sqlite3.connect(db_path)
-        cursor = workflow_db.cursor()
 
         for deal in storage_deals:
 
@@ -51,8 +42,3 @@ if total_deals > 0:
             cursor.execute("SELECT name from packages where cid = ?", (cid,),)
             filename = cursor.fetchone()
             csvwriter.writerow([deal["dealInfo"]["dealId"], deal["rootCid"], deal["dealInfo"]["miner"], filename[0], deal["dealInfo"]["size"], utc_date, deal["dealInfo"]["pieceCid"], price, deal["dealInfo"]["startEpoch"], deal["dealInfo"]["activationEpoch"], deal["dealInfo"]["duration"], deal["address"], deal["dealInfo"]["stateName"]])
-            cursor.execute("INSERT OR IGNORE INTO deals (deal_id, payload_cid, piece_cid, timestamp, piece_size, miner_id, start_epoch, activation_epoch, duration, price, wallet, state) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", (
-                deal["dealInfo"]["dealId"], deal["rootCid"], deal["dealInfo"]["pieceCid"], utc_date, deal["dealInfo"]["size"], deal["dealInfo"]["miner"], deal["dealInfo"]["startEpoch"], deal["dealInfo"]["activationEpoch"], deal["dealInfo"]["duration"], price, deal["address"], deal["dealInfo"]["stateName"]),)
-            workflow_db.commit()
-
-    workflow_db.close()

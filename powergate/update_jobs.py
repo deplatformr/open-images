e3eced724292bda0b8330196e0407194622febaf
Peter Van Garderen <peter@artefactual.com>
2020-12-01 17:12:33 -0800

    WIP: update jobs. re-org directory.

--- powergate/update_jobs.py
+++ powergate/update_jobs.py
@@ -1,44 +1,33 @@
-import os
-import sqlite3
 from pygate_grpc.client import PowerGateClient
 from google.protobuf.json_format import MessageToDict
-
+import os
+import sqlite3
 
 api = os.getenv('POWERGATE_API')
-ffs = os.getenv('POWERGATE_FFS')
 token = os.getenv('POWERGATE_TOKEN')
-
+user = os.getenv('POWERGATE_USER')
 
 powergate = PowerGateClient(api, is_secure=False)
 
+queued_jobs = powergate.storage_jobs.queued(cids='', token=token)
+executing_jobs = powergate.storage_jobs.executing(cids='', token=token)
 
-abs_path = os.getcwd()
-split = os.path.split(abs_path)
-workflow_db_path = os.path.join(
-    split[0], "pipeline/deplatformr_open_images_workflow.sqlite")
+executing = MessageToDict(executing_jobs)
+queued = MessageToDict(queued_jobs)
 
-workflow_db = sqlite3.connect(workflow_db_path)
-cursor = workflow_db.cursor()
+print(str(len(executing["storageJobs"])) + " jobs currently executing.")
+print(str(len(queued["storageJobs"])) + " jobs currently queued.")
 
-cursor.execute("SELECT name, cid FROM packages WHERE cid IS NOT NULL")
-packages = cursor.fetchall()
 
-for package in packages:
-    cursor.execute("SELECT job_id FROM jobs WHERE cid=?", (package[1],),)
-    jobs = cursor.fetchall()
-    print(package[0] + "(CID: " + package[1] +
-          ") has " + str(len(jobs)) + " jobs.")
-    for job in jobs:
-        job_id = job[0]
-        print("Job ID: " + job_id)
-        try:
-            response = powergate.ffs.get_storage_job(
-                jid=job_id, token=token)
-            job = MessageToDict(response)
-            status = job["job"]["status"]
-        except Exception as e:
-            status = "JOB_STATUS_NOT_FOUND"
-        print("Status: " + status)
-        cursor.execute(
-            "UPDATE jobs SET status=? WHERE job_id=?", (status, job_id, ), )
-        workflow_db.commit()
+"""
+
+cancel_job = powergate.admin.storage_jobs.cancel(user_id=user, cids=['3b84a9e2-c826-4b38-8144-9262665e8544]'])
+
+print(cancel_job)
+
+job_msg = powergate.admin.storage_jobs.executing(user_id=user, cids=['QmPTpqgybVctWdvxLP6VJP82bSfzz3hAh68zxeU9YU6npA'])
+
+job = MessageToDict(job_msg)
+
+print(job)
+"""

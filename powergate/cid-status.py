33174760b3bc3de98c9cf3a8dce82c639fb0506c
Peter Van Garderen <peter@artefactual.com>
2020-12-02 17:26:19 -0800

    remove sample output used for debug

--- powergate/cid-status.py
+++ powergate/cid-status.py
@@ -29,21 +29,3 @@ if __name__ == "__main__":
     except Exception as e:
         print("Unable to get status for " + str(sys.argv[1]) + ".")
         print(e)
-
-"""
-CidInfo
-    (cid='QmRpALcLgVYvyurJqNR59WEyyA4wS1hDQYK4SQ48MaCqQR', 
-    latestPushedStorageConfig={'hot': {'allowUnfreeze': True, 'unfreezeMaxPrice': '50000000', 'ipfs': {'addTimeout': '30'}}, 'cold': {'enabled': True, 'filecoin': {'replicationFactor': '4', 'dealMinDuration': '518400', 'renew': {}, 'address': 'f3qne2hc6e6hoki2o47urwodci3546z2govz6zjachm3yeogj6rfqyoh2xticeuwwt7rk6yfbit7xvdiukt4tq', 'maxPrice': '10000000000', 'fastRetrieval': True, 'dealStartOffset': '8640'}}, 'repairable': True}, executingStorageJob=
-        {'id': '30afaab2-82ae-478d-b081-27bcd32d5f04', 'apiId': '7bcc30a1-fecc-4892-a99a-ab6c54d59360', 'cid': 'QmRpALcLgVYvyurJqNR59WEyyA4wS1hDQYK4SQ48MaCqQR', 'status': 'JOB_STATUS_EXECUTING',          
-            'dealInfo': 
-                [{'proposalCid': 'bafyreiadikthr23a3tvszap7deeswixqivkkthpgvpkqz5tflkf5vcvlwa', 'stateId': '13', 
-                'stateName': 'StorageDealCheckForAcceptance', 
-                'miner': 'f080480', 
-                'pieceCid': 'baga6ea4seaqpabshwzcrmt2fcumeii22kfg4crpsh4wwyaqps5mycociexhkija', 
-                'size': '1065353216', 
-                'pricePerEpoch': '10000000000', 
-                'duration': '521721'}, 
-                {'proposalCid': 'bafyreibl4xfcdkzt5kv6ymdchpn7rx7tafxoahmuikphezsic4lnseews4', 'stateId': '13', 'stateName': 'StorageDealCheckForAcceptance', 'miner': 'f014768', 'pieceCid': 'baga6ea4seaqpabshwzcrmt2fcumeii22kfg4crpsh4wwyaqps5mycociexhkija', 'size': '1065353216', 'pricePerEpoch': '5000000000', 'duration': '523159'}, 
-                {'proposalCid': 'bafyreibragoucbw62wpqhiui23w6567lo6m2xly3uzz5br7lnzwz3ureh4', 'stateId': '13', 'stateName': 'StorageDealCheckForAcceptance', 'miner': 'f079817', 'pieceCid': 'baga6ea4seaqpabshwzcrmt2fcumeii22kfg4crpsh4wwyaqps5mycociexhkija', 'size': '1065353216', 'pricePerEpoch': '10000000000', 'duration': '523805'}, 
-                {'proposalCid': 'bafyreidqfte5vmf2emvufnzcvlveal4s67atybn7b2uyvmcytrmqowiejm', 'stateId': '16', 'stateName': 'StorageDealStartDataTransfer', 'miner': 'f065280', 'pieceCid': 'baga6ea4seaqpabshwzcrmt2fcumeii22kfg4crpsh4wwyaqps5mycociexhkija', 'size': '1065353216', 'pricePerEpoch': '500000000', 'duration': '522933'}], 'createdAt': '1606955873'}, queuedStorageJobs=None)
-"""
